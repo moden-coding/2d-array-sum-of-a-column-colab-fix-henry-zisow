8ae28435211d5cca5795efd9f6a1c806d63f2a09
HenryZisow <28hzisow@gannacademy.org>
2026-01-16 12:36:20 -0500

    Done

--- src/App.java
+++ src/App.java
@@ -1,16 +1,20 @@
 public class App {
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		int[][] a = {
-			{-5,-2,-3,7},
-			{1,-5,-2,2},
-			{1,-2,3,-4}
+				{ -5, -2, -3, 7 },
+				{ 1, -5, -2, 2 },
+				{ 1, -2, 3, -4 }
 		};
-		System.out.println(sumColumn(a,1)); //should print -9
-		System.out.println(sumColumn(a,3)); //should print 5
+		System.out.println(sumColumn(a, 1)); // should print -9
+		System.out.println(sumColumn(a, 3)); // should print 5
 	}
-	public static int sumColumn(int[][] array, int column)
-	{
-		return -1; //placeholder
+
+	public static int sumColumn(int[][] array, int column) {
+		int sum = 0;
+		for (int i = 0; i < array.length; i++) {
+			sum = sum + array[i][column];
+		}
+		return sum;
 	}
-	
+
 }
